5925b5fd60e6338c8b42edbe09bf441b851220e7
kennethezell <KennethEzell@hotmail.com>
2024-06-10 09:11:26 -0400

    updating directory

---
+++ testtest.py
@@ -0,0 +1,67 @@
+#integer Division
+
+# print(10 // 3)
+# print(4 // 3)
+
+# Using variables as placeholders
+num_one = 1
+num_two = 20
+
+
+# print(num_one + num_two)
+
+# Progtam to find the perimeter of a rectangle
+# perimeter = 2 * (length + width)
+
+# declaring your variables
+length = 10
+width = 7
+
+# perimeter = 2 * (length +width)
+
+# print("The perimeter of my revtangle is", perimeter)
+
+ # Data types
+ # integer
+num_3 = 5
+# print(num_3)
+# print(type(num_3_))
+
+# String
+# fav_color
+
+# Lists
+# students_grades = [100, 95, 70, 85, 40]
+# # print(students_grades)
+
+# demographic_info
+
+# caastering an integer to a string
+second_num = 10
+print(type(second_num))
+
+new_string =str(second_num)
+# print(type(new_string))
+
+# Colors
+# My_fav_colors = ['blue', 'black',"red",'purple','yellow']
+
+# # Eval
+
+# cold_weather = 'True'
+# print(eval(cold_weather))
+
+# sidE_one = 6
+# side_two = 7
+# base = 
+
+# convert fareenheit to celcius
+
+# F = 100
+# celcius = ((F -32) - 5/9))
+# print (celcius)
+
+RAD = 5
+HEI = 10
+v = 3.014 * (RAD * RAD) * HEI
+
