3efc3f35986eb8a2fdb9cdcd0b0b11ba81fdf3f5
kennethezell <KennethEzell@hotmail.com>
2024-06-10 11:13:51 -0400

    more data tyes

--- class2-print-variable-operators/class_2_variables_and_operators.py
+++ class2-print-variable-operators/class_2_variables_and_operators.py
@@ -60,6 +60,51 @@ num_one = 1
 num_two = 20
 
 # print(num_one)
-print(num_one + num_two)
+# print(num_one + num_two)
+
+# Program to find the perimeter of a rectable
+# perimenter = 2 * (length + width)
+
+# declariong your variable
+length = 10
+width = 7
+
+# perimeter = 2 * (length +width)
+
+# print(perimeter,length,width)
+
+# print('The perimeter of my rectangle is',perimeter)
 
 
+# Data Typeas
+
+# Integer
+num_3 = 5 # declare variable
+
+# print(num_3) # print variable value
+# print(type(num_3)) # we can check data type
+
+# Strings
+fav_color = 'blue'
+# print(fav_color)
+# print(type(fav_color))
+
+# Boolean
+technical_errors = True
+print(technical_errors)
+print(type(technical_errors))
+
+# Float
+
+num_4 = 4.05
+# print (num_4)
+# print(type(num_4))
+
+# List
+students_grades =[100, 95, 70, 85, 40]
+# print(students_grades)
+# print (type(students_grades))
+
+# For loop
+for s in students_grades: # s is a temporary variable for each student grade in the list
+    print('One grade is', s)
