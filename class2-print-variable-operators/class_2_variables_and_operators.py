070e6f3ee8f543b162959d92761ce02755ee7b1a
kennethezell <KennethEzell@hotmail.com>
2024-06-10 10:01:30 -0400

    adding operators to notes

---
+++ class2-print-variable-operators/class_2_variables_and_operators.py
@@ -0,0 +1,42 @@
+# Let's talk about the print statement
+# I am a comment
+
+# print('Hello World') # Print built in function
+
+
+
+
+# Addition
+# print(4 + 2)
+# print (7 +10)
+# print(100 + 200)
+
+# Subtraction
+
+
+# print(10 - 7)
+# print(4 - 3)
+# print(9 - 8)
+
+# Multiplication
+
+# print(4 * 8)
+# print(3 * 9)
+# print(11 * 9)
+
+# Division
+
+# print(10 / 2)
+# print(5 / 0)
+
+# Try Except
+
+# try:
+#     print(5/0)
+# except ZeroDivisionError as e:
+#     print("You cannot divide by 0")
+
+# Exponents
+# print(5 ** 5)
+# print(2 ** 5)
+# print(3 ** 6)
---
+++ class2-print-variable-operators/test.py
@@ -0,0 +1,2 @@
+# Let's talk about the print statement
+# I am a comnmnent
