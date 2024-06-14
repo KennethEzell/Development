bdc850a460b9a636cd66dbeb064883d7dad7aa58
kennethezell <KennethEzell@hotmail.com>
2024-06-14 14:45:59 -0400

    Assignment_2-Area_of_a_Triangle

---
+++ class4-strings-pt1/Assignment 6-14.py
@@ -0,0 +1,21 @@
+""" Calculate the area of a Triangle 
+Area of Triange = 1/2 x base x height 
+base = 20 height = 15
+
+Requirements
+-Comment your code
+-use proper naming for your variables
+-Include a string within your print built in function"""
+
+# Set base and height values
+Base = 20
+Height = 15
+# Establish the variable - Area_of_Triangle
+Area_of_Triangle = (Base*Height)/2
+
+# print(Area_of_Triangle) - Tested validity of variable
+
+# String with variable built in
+print("The Area of the Triangle is", Area_of_Triangle) 
+
+
