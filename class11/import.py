1a88d30ad756b81a0910db978f46f3534db57d0f
kennethezell <KennethEzell@hotmail.com>
2024-07-13 14:39:35 -0400

    successful import export of excel

--- class11/import.py
+++ class11/import.py
@@ -1,10 +1,10 @@
 import os
 import pandas as pd
 
-# print("my current working directory", os.getcwd())
+print("my current working directory", os.getcwd())
 
-# os.chdir ("C:\SUMMER-2024-PROGRAMMING-CONCEPTS\class11_list_pt2")
-# print('my lists part 2 folder has a new cmd', os.getcwd())
+os.chdir ("C:\SUMMER-2024-PROGRAMMING-CONCEPTS\class11_list_pt2")
+print('my lists part 2 folder has a new cmd', os.getcwd())
 
 baseline = pd.read_csv('emails.csv')
 
@@ -12,6 +12,6 @@ baseline = pd.read_csv('emails.csv')
 
 baseline['uni-id'] = baseline['uni-id'] + '@gmail.com'
 
-# print(baseline)
+print(baseline)
 
 baseline.to_excel('output.xlsx', index=False)
---
+++ class11_list_pt2/emails.py
@@ -0,0 +1,10 @@
+import os
+import pandas as pd
+
+print('My current working directory', os.getcwd())
+
+os.chdir('C:\SUMMER-2024-PROGRAMMING-CONCEPTS\class11_list_pt2')
+
+print('My lists part 2 folder has a new cwd', os.getcwd())
+
+
