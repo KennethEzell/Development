a788501279830723aedbb0a5cc45cff6510e8f51
kennethezell <KennethEzell@hotmail.com>
2024-07-14 21:38:03 -0400

    userpassword in progress

--- Project_1/proj1.template.py
+++ Project_1/proj1.template.py
@@ -14,7 +14,12 @@ We will need 2 variables to capture the username and password. Another 2 variabl
 # username
 
 ''' A List to handle error messages '''
-error_msgs = ["Username must begin with a lowercase. Please try again. ", "That username is already taken. Please try again.", "Username must be alphanumeric."]
+error_msgs = ["Username must begin with a lowercase. Please try again. ", 
+              "That username is already taken. Please try again.", 
+              "Username must be alphanumeric.", 
+              "Password must be at least 8 characters long. ",
+              "Password must contain at least one uppercase letter. "
+              ]
 taken_user_names = ['admin', 'admin123','user1','superuser']
 
 ''' Start your while loop '''
@@ -27,22 +32,36 @@ while True:
 
 #''' Test your username and enforce logic'''
 
-    if not username.islower():
+    if not username.islower(): # check first character is lower case, return error message if not
         print(error_msgs[0])     
         continue
-    elif username in taken_user_names:
+    elif username in taken_user_names: # check if username is in the reserved list. Return error if so
         print(error_msgs[1])
         continue
-    elif not username.isidentifier():
+    elif not username.isidentifier(): # check if username contains alphanumeric or '_'. Return error if not
         print(error_msgs[2])
         continue
-
-
-    userpassword = input('Please enter your password: ')
-    print(userpassword)
-
     break
 
+while True:
+
+# Get userpassword
+    userpassword = input('Please enter your password: ')
+
+    if len(userpassword) < 8:
+        print(error_msgs[3])
+        continue
+    if userpassword.lower() == userpassword:
+        print(error_msgs[4])
+        continue
+    else:
+        print('Good job')
+        break
+
+
+    break
+print(username)
+print(userpassword)
 
 
 
