0698fd76012e3ed5d6299d33883354b50f5c991f
kennethezell <KennethEzell@hotmail.com>
2024-08-16 21:10:38 -0400

    brainteasers final

--- class21_brain_teasers/brainteasers.py
+++ class21_brain_teasers/brainteasers.py
@@ -137,14 +137,16 @@ into this:
 {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7, 'i': 8, 'j': 9, 'k': 10, 'l': 11, 'm': 12, 'n': 13, 'o': 14, 'p': 15, 'q': 16, 'r': 17, 's': 18, 't': 19, 'u': 20, 'v': 21, 'w': 22, 'x': 23, 'y': 24, 'z': 25}
 '''
 
-convert_me = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+# To be honest, this one took multiple google searches. I'm going over the dictionary class again this weekend #
 
-temp_list = ()
-into_dictionary = {}
+# convert_me = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-convert_me_dic = {char: index for index, char in enumerate(convert_me)}
+# temp_list = ()
+# into_dictionary = {}
 
-print(convert_me_dic)
+# convert_me_dic = {char: index for index, char in enumerate(convert_me)}
+
+# print(convert_me_dic)
 
 
 
