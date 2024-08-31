13901b5abc4d9ca51e87e1e8f9abd2966b69ac46
kennethezell <KennethEzell@hotmail.com>
2024-08-31 13:00:17 -0400

    completed project 2

--- Project_2/Main.py
+++ Project_2/Main.py
@@ -1,14 +1,32 @@
 from Module import Employee
 
+# establish values to the attributes for the Module file
 emp1 = Employee('Kenneth Ezell', 'Analyst', 'Transportation', 100000, 2010)
-# print(emp1)
-# emp1.years_worked()
-# print(emp1.total_expense())
-# emp1.print_employee_information()
-print(emp1.get_name(), emp1.get_job_title(), emp1.get_department(), emp1.get_salary(), emp1.get_hire_year())
-emp1.set_name('Chris')
-print(emp1.name)
-emp1.set_job_title('Buster')
-emp1.set_salary(200000)
+print(emp1)
 
+# test years worked function
+emp1.years_worked()
+
+# test total expense function
+print(emp1.total_expense())
+
+# test print employee information function output to txt file in employee's name
+emp1.print_employee_information()
+
+# test accessor functions for data. I placed all of them into one print statement since they are only accessors
 print(emp1.get_name(), emp1.get_job_title(), emp1.get_department(), emp1.get_salary(), emp1.get_hire_year())
+
+# below are the mutator functions to change the data attributes
+emp1.set_name('Rebecca Ezell')
+emp1.set_job_title('Queen of the House')
+emp1.set_department('House Affairs')
+emp1.set_salary(120000)
+emp1.set_hire_year(2006)
+
+# display updated data from the mutator functions
+print(emp1)
+
+# create new txt file in new employee name and info
+emp1.print_employee_information()
+
+
--- Project_2/Module.py
+++ Project_2/Module.py
@@ -1,4 +1,4 @@
-
+'''My import statements to pull functions necessary for calculations'''
 
 from datetime import datetime
 import math
@@ -6,9 +6,10 @@ import pandas
 import csv
 import os
 
+# Set local directory for export of txt file
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
-
+'''Initialize the class "Employee" to capture the attributes from the Main file and initialize the variables. '''
 class Employee(object):
 
     def __init__(self, name: str, job_title: str, department: str, salary: float, hire_year: int):
@@ -18,16 +19,22 @@ class Employee(object):
         self.salary = salary
         self.hire_year = hire_year
 
+    '''Below are the requested Methods to view and manipulate the attribute data'''
+
+    # string representation of the data
     def __str__(self):
         return f'{self.name} {self.job_title} {self.department} {self.salary} {self.hire_year}'
     
+    # function to print the total number of years worked in a string. (current date - hire year)
     def years_worked(self):
         print(f'Years worked: {datetime.now().year - self.hire_year}')
 
+    # function to caluculate total expense of employee and return the answer in a string. (years worked * salary)
     def total_expense(self):        
         emp_total_expense = (datetime.now().year - self.hire_year) * self.salary
         return f'Total expense: {(datetime.now().year - self.hire_year) * self.salary}'
 
+    # function to print all of the employee information into a txt file named after said employee (replacing spaces with "_")
     def print_employee_information(self):
         filename = f"{self.name.replace(' ', '_')}.txt"
         with open(filename, 'w') as file:
@@ -37,8 +44,7 @@ class Employee(object):
             file.write(f"Salary: {self.salary}\n")
             file.write(f"Year Hired: {self.hire_year}\n")
             
-
-    # Accessor methods (getters)
+    # Accessor methods (getters) for the attributes in the main file
     def get_name(self):
         return self.name
 
@@ -55,7 +61,7 @@ class Employee(object):
         return self.hire_year 
     
 
-    # Mutator methods (setters)
+    # Mutator methods (setters) for the attributes in the main file
     def set_name(self, name):
         self.name = name
 
@@ -72,36 +78,3 @@ class Employee(object):
         self.hire_year = hire_year
     
     
-
-
-    # def set_job_title(self, job_title):
-    #     if isinstance(job_title, str):
-    #         self._job_title = job_title
-    #     else:
-    #         raise ValueError("Job title must be a string.")
-
-    # def set_department(self, department):
-    #     if isinstance(department, str):
-    #         self._department = department
-    #     else:
-    #         raise ValueError("Department must be a string.")
-
-    # def set_salary(self, salary):
-    #     if isinstance(salary, (int, float)) and salary >= 0:
-    #         self._salary = float(salary)
-    #     else:
-    #         raise ValueError("Salary must be a non-negative number.")
-
-    # def set_hire_year(self, hire_year):
-    #     if isinstance(hire_year, int) and hire_year > 0:
-    #         self._hire_year = hire_year
-    #     else:
-    #         raise ValueError("Hire year must be a positive integer.")
-
-# with open('animals.txt', 'w') as file:
-#     file.writelines(animals)
-
-
-# df = pd.DataFrame.from_dict(data)
-
-# df.to_csv('output.csv', index=False)
